424ecafdd5e1396dab2ea82c0680fa296459f655
swastikk06 <royswastikk06@gmail.com>
2020-02-02 10:27:47 +0530

    debuging

--- sketch.js
+++ sketch.js
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -94,7 +94,10 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+        Matter.Body.setPosition(bird.body,{x:200,y:50});
        slingshot.attach(bird.body);
+       bird.trajectory=[];
+       gameState="onSling"
     }
 }
 
